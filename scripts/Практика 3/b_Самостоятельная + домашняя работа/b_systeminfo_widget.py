85bed0f134c8c72c6eb04e6863378fb86e0bd713
Egor <egorobidin@inbox.ru>
2022-12-22 17:00:44 +0300

    12

--- scripts/Практика 3/b_Самостоятельная + домашняя работа/b_systeminfo_widget.py
+++ scripts/Практика 3/b_Самостоятельная + домашняя работа/b_systeminfo_widget.py
@@ -11,3 +11,63 @@
 5. установку времени задержки сделать "горячей", т.е. поток должен сразу
 реагировать на изменение времени задержки
 """
+
+import time
+import psutil
+
+from PySide2 import QtCore, QtWidgets
+from ui.cpu import Ui_Form
+from a_threads import SystemInfo
+
+class QThreadPractice(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.systemInfoThread = None
+
+        self.initThreads()
+        self.initSignals()
+
+    def initThreads(self):
+
+        self.systemInfoThread = SystemInfo()
+        self.systemInfoThread.systemInfoReceived.connect(self.systemInfoThreadSystemSignal)
+        self.systemInfoThread.start()
+
+    def initSignals(self):
+
+        self.ui.spinBoxSystemInfoDelay.valueChanged.connect(self.setSystemInfoDelay)
+
+    def systemInfoThreadSystemSignal(self, info_list) -> None:
+        """
+        Слот для обработки сигнала systemSignal, который шлёт данные из потока self.systemInfo
+
+        :param info_list: значение полученное из потока self.systemInfo
+        :return: None
+        """
+
+        self.ui.progressBarCPU.setValue(info_list[0])
+        self.ui.labelCPUPercent.setText(f"{info_list[0]} %")
+        self.ui.progressBarRAM.setValue(info_list[1].percent)
+        self.ui.labelRAMPercent.setText(f"{info_list[1].percent} %")
+
+    def setSystemInfoDelay(self) -> None:
+        """
+        Слот для установки значения времени задержки обновления системных параметров
+
+        :return: None
+        """
+
+        self.systemInfoThread.delay = self.ui.spinBoxSystemInfoDelay.value()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    myapp = QThreadPractice()
+    myapp.show()
+
+    app.exec_()
